48e4756e999180ad3ad9036ea22c06f250ca3783
Mary West <mwest.lazerade@gmail.com>
2021-01-31 15:55:22 -0700

    Try again

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
-namespace SOFTWEAR
+//% color=#f44242 icon="\u26C8"
+namespace softwear
 {
-  //% weight=41 blockGap=8 blockId="SOFTWEAR:motion Accel x" block="accelX"    
+  //% weight=41 blockGap=8 blockId="accelX" block="accelX"    
   export function accelX(): number
   {
     return 0
