fe3a1fb0eeb1af0ac1f9952d18f66f59b52f0060
Mary West <mwest.lazerade@gmail.com>
2021-01-31 16:32:24 -0700

    Use good values

--- main.ts
+++ main.ts
@@ -120,20 +120,20 @@ namespace softwear
   const LSM6DS3_ACC_GYRO_MAG_OFFZ_L                    = 0x31
   const LSM6DS3_ACC_GYRO_MAG_OFFZ_H                    = 0x32
   
-	  /**
+    /**
      * Writes a value to a register on the CCS811 Air Quality Sensor
      */
     function writeCCSReg(reg: number, val: number): void {
         let test = reg << 8 | val
-        pins.i2cWriteNumber(ccsAddr, reg << 8 | val, NumberFormat.Int16BE)
+        pins.i2cWriteNumber(LSM6DS3Addr, reg << 8 | val, NumberFormat.Int16BE)
     }
 
-	  /**
+    /**
      * Reads a value from a register on the CCS811 Air Quality Sensor
      */
     function readCCSReg(reg: number, format: NumberFormat) {
-        pins.i2cWriteNumber(ccsAddr, reg, NumberFormat.UInt8LE, false)
-        let val = pins.i2cReadNumber(ccsAddr, format, false)
+        pins.i2cWriteNumber(LSM6DS3Addr, reg, NumberFormat.UInt8LE, false)
+        let val = pins.i2cReadNumber(LSM6DS3Addr, format, false)
         return val
     }
   
