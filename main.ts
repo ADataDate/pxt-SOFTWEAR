93ebaef6fe0156ce4b4607837912b03fadfea51f
Mary West <mwest.lazerade@gmail.com>
2021-01-31 16:23:32 -0700

    Change X to x in HEX

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 //% color=#f44242 icon="\u26C8"
 namespace softwear
 {
-  const LSM6DS3Addr                        = 0x6B
+  const LSM6DS3Addr                       = 0x6B
+  const LSM6DS3_ACC_GYRO_TEST_PAGE  			= 0x00
 
   //% weight=41 blockGap=8 blockId="accelX" block="accelX"    
   export function accelX(): number
