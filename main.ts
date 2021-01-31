ff00cec6ad710c4aac33d100353b02f118d5d2ae
Mary West <mwest.lazerade@gmail.com>
2021-01-31 14:03:32 -0700

    Testing

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
 
+let pressure_one = 0
+let pressure_2 = 0
+let delta_pressure = 0
+let inWC = 0
+forever(function () {
+    pixel.setColor(0x0000ff)
+    pause(1000)
+    pixel.setColor(0xff0000)
+    pause(100)
+    bme280.setAddress(BME280_I2C_ADDRESS.ADDR_0x76)
+    pressure_one = bme280.pressure()
+    bme280.setAddress(BME280_I2C_ADDRESS.ADDR_0x77)
+    pressure_2 = bme280.pressure()
+    delta_pressure = pressure_one - pressure_2
+    inWC = delta_pressure * 0.0040146307866177
+    display.showNumber(inWC, 1)
+})
